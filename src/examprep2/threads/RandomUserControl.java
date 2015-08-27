3826aa3f363492d32ab939ae33d71b87ccc45168
Alexander Steen <ale.steen@hotmail.com>
2015-08-27 21:33:26 +0200

    Exercise completed

--- src/examprep2/threads/RandomUserControl.java
+++ src/examprep2/threads/RandomUserControl.java
@@ -1,21 +1,36 @@
 package examprep2.threads;
 
-
 import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import randomperson.RandomUser;
 import randomperson.RandomUserGenerator;
 
-public class RandomUserControl {
-  
-  public RandomUser fetchRandomUser() {
-   RandomUser user= null;
-    try {
-      user = RandomUserGenerator.getRandomUser();
-    } catch (InterruptedException ex) {
-      Logger.getLogger(RandomUserControl.class.getName()).log(Level.SEVERE, null, ex);
+public class RandomUserControl extends Observable implements Runnable{
+
+    private RandomUser user;
+
+    public RandomUserControl() {
+        this.user = null;
     }
-   return user;
-  }
+    
+    public void fetchRandomUser() {
+        try {
+            user = RandomUserGenerator.getRandomUser();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RandomUserControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        setChanged();
+        notifyObservers(this);
+    }
+
+    public RandomUser getUser() {
+        return user;
+    }
+
+    @Override
+    public void run() {
+        fetchRandomUser();
+    }
+    
 }
